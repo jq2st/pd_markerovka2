1b1c24e3d4a0953705c54e23b0e5fe31b3b43fa2
jq2st <igaaaaan7@gmail.com>
2021-04-11 08:41:46 +0300

    back uploadv2

---
+++ server/middleware/upload.js
@@ -0,0 +1,16 @@
+const multer = require('multer')
+const moment = require('moment')
+
+const storage = multer.diskStorage({
+    destination(req, file, cb) {
+        cb(null, 'uploads/')
+    },
+    filename(req, file, cb) {
+        const date = moment().format('DDMMYYYY-HHmmss_SSS')
+        cb(null, date + '_' + file.originalname)
+    }
+})
+
+module.exports = multer({
+    storage: storage
+})
