9d0b57b8c4081f3b4289eae731022ef62e02ce27
jq2st <igaaaaan7@gmail.com>
2021-04-17 01:04:02 +0300

    steady

--- src/app/history-page/history-page.component.ts
+++ src/app/history-page/history-page.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../services/api.service';
 
@@ -9,8 +10,15 @@ import { ApiService } from '../services/api.service';
 export class HistoryPageComponent implements OnInit {
 
   labels = []
+  compare = {
+    1: 'Изъятие нескольких уровней в диапазоне канала',
+    2: 'Введение штрихового кода в тоновый диапазон канала RGB',
+    3: 'Введение 2D штрихового кода (QR) в цифровое изображение',
+    4: 'Изъятие 20-30 уровней в диапазоне',
+    5: 'Введение флуоресцентного тонера'
+  }
 
-  constructor(private api: ApiService) { }
+  constructor(private api: ApiService, private http: HttpClient) { }
 
   ngOnInit() {
     this.api.getHistory()
@@ -20,4 +28,7 @@ export class HistoryPageComponent implements OnInit {
       })
   }
 
+  getFile(file) {
+    window.open(file)
+  }
 }
